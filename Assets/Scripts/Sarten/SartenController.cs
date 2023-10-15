a73972174468e9d0dbee26d3c33e8b06fe453f91
Alejandro <alejandrong904@gmail.com>
2023-10-15 16:50:16 +0200

    Uso scripts propios lmao

--- Assets/Scripts/Sarten/SartenController.cs
+++ Assets/Scripts/Sarten/SartenController.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 
 public class SartenController : MonoBehaviour
 {
+    public GameObject pala1, pala2;
+    private Vector3 pala1Pos, pala2Pos;
+    
+    private List<GameObject> players = new List<GameObject>();
+    
     // public bool isCooking = false;
     public bool bothInside = false;
     
     public int playerCount = 0;
+    
+    private void Start()
+    {
+        pala1Pos = pala1.transform.position;
+        pala2Pos = pala2.transform.position;
+    }
+    
+    private void Update()
+    {
+        if (!bothInside)
+        {
+            pala1.transform.position = Vector3.Lerp(pala1.transform.position, pala1Pos, Time.deltaTime);
+            pala2.transform.position = Vector3.Lerp(pala2.transform.position, pala2Pos, Time.deltaTime);
+        }
+        else
+        {
+            pala1.transform.position = Vector3.Lerp(pala1.transform.position, players[0].transform.position + new Vector3(0, 0.25f, 0), Time.deltaTime);
+            pala2.transform.position = Vector3.Lerp(pala2.transform.position, players[1].transform.position + new Vector3(0, 0.25f, 0), Time.deltaTime);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            players.Add(other.gameObject);
             playerCount++;
             if (playerCount == 2)
             {
@@ -26,6 +52,7 @@ public class SartenController : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            players.Remove(other.gameObject);
             playerCount--;
             if (playerCount != 2)
             {
