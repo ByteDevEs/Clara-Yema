70167d3e2837be2e565363ebda52243b52c26c59
Alejandro <alejandrong904@gmail.com>
2023-11-08 14:59:26 +0100

    Arreglado problema al clickar en el boss

--- Assets/Scripts/Sarten/BossHealthController.cs
+++ Assets/Scripts/Sarten/BossHealthController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,17 @@ using UnityEngine.UI;
 
 public class BossHealthController : HealthController
 {
-    [SerializeField] Slider healthBar;
+    [SerializeField] GameObject bossCanvasPrefab;
+    GameObject bossCanvas;
+    
+    Slider healthBar;
+
+    private void Start()
+    {
+        bossCanvas = Instantiate(bossCanvasPrefab);
+        healthBar = bossCanvas.GetComponentInChildren<Slider>();
+    }
+
     // Update is called once per frame
     void Update()
     {
