9a563f70c2b780cac26a4f3fd57c8db641353668
VonP2 <adrian11soriano@gmail.com>
2023-11-08 19:32:08 +0100

    Arreglado error en hundir

--- Assets/Scripts/Hundir.cs
+++ Assets/Scripts/Hundir.cs
@@ -8,7 +8,7 @@ public class Hundir : MonoBehaviour
     public float riseSpeed = 1.0f; // Velocidad de ascenso.
     public float sinkDepth = -5.0f; // Profundidad a la que la plataforma se hunde.
     private Vector3 originalPosition; // Posición original de la plataforma.
-    private bool isSinking = false; // Indica si la plataforma se está hundiendo.
+    private int playerCount = 0; // Contador de jugadores en la plataforma.
 
     private void Start()
     {
@@ -18,14 +18,14 @@ public class Hundir : MonoBehaviour
     private void Update()
     {
         // Si la plataforma está hundiéndose y no ha alcanzado la profundidad deseada, sigue hundiéndose.
-        if (isSinking && transform.position.y > sinkDepth)
+        if (playerCount > 0 && transform.position.y > sinkDepth)
         {
             Vector3 newPosition = transform.position;
             newPosition.y -= sinkSpeed * Time.deltaTime;
             transform.position = newPosition;
         }
         // Si la plataforma no está hundiéndose y no ha alcanzado su posición original, sube poco a poco.
-        else if (!isSinking && transform.position.y < originalPosition.y)
+        else if (playerCount == 0 && transform.position.y < originalPosition.y)
         {
             Vector3 newPosition = transform.position;
             newPosition.y += riseSpeed * Time.deltaTime;
@@ -35,19 +35,19 @@ public class Hundir : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Cuando un personaje colisiona con la plataforma, inicia el hundimiento.
+        // Cuando un personaje colisiona con la plataforma, aumenta el contador.
         if (other.CompareTag("Player"))
         {
-            isSinking = true;
+            playerCount++;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // Cuando un personaje deja de colisionar con la plataforma, detiene el hundimiento.
+        // Cuando un personaje deja de colisionar con la plataforma, disminuye el contador.
         if (other.CompareTag("Player"))
         {
-            isSinking = false;
+            playerCount--;
         }
     }
 }
