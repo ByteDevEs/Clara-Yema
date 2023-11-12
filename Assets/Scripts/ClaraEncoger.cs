2ff42f3faa5b827c536235cb8b5c9498f2066ce6
VonP2 <adrian11soriano@gmail.com>
2023-11-12 15:27:39 +0100

    Clara tiene la habilidad de encoger +-

---
+++ Assets/Scripts/ClaraEncoger.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ClaraEncoger : MonoBehaviour
+{
+    // Factor de escala para el collider vertical
+    public float verticalScale = 0.5f;
+
+    // Referencia al collider del personaje
+    private CapsuleCollider characterCollider;
+
+    // Referencia al transform del modelo del personaje
+    private Transform characterModelTransform;
+
+    PlayerController moveScript;
+
+    private bool isShrinking = false;
+
+    void Start()
+    {
+
+        // Obtener el collider del personaje (asegúrate de que el GameObject tenga un characterCollider adjunto)
+        characterCollider = GetComponent<CapsuleCollider>();
+        // Obtener el transform del modelo del personaje
+        characterModelTransform = transform.GetChild(0);
+
+        moveScript = GetComponent<PlayerController>();
+
+        if (characterCollider == null)
+        {
+            Debug.LogError("Este script requiere un BoxCollider2D en el GameObject.");
+        }
+
+        if (characterModelTransform == null)
+        {
+            Debug.LogError("No se encontró el modelo del personaje. Asegúrate de que el modelo sea el primer hijo del GameObject.");
+        }
+    }
+    public void OnCircle(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            // Comenzar la rutina de encogimiento cuando el botón se presiona
+            StartCoroutine(Shrink());
+        }
+        else if (context.canceled)
+        {
+            // Detener el encogimiento cuando el botón se suelta
+            StopCoroutine(Shrink());
+            isShrinking = false;
+
+            // Volver al tamaño normal
+            ResetSize();
+        }
+    }
+    IEnumerator Shrink()
+    {
+        // Evitar que se inicie múltiples veces mientras el botón sigue presionado
+        if (!isShrinking)
+        {
+            isShrinking = true;
+
+            // Encoger
+            Vector3 originalScale = characterCollider.transform.localScale;
+            characterCollider.transform.localScale = new Vector3(originalScale.x, originalScale.y * verticalScale, originalScale.z);
+
+            Vector3 scale = characterModelTransform.localScale;
+            scale.y *= verticalScale;
+            characterModelTransform.localScale = scale;
+        }
+
+        yield return null;
+
+    }
+
+    void ResetSize()
+    {
+        // Restablecer al tamaño normal
+        Vector3 originalScale = characterCollider.transform.localScale;
+        characterCollider.transform.localScale = new Vector3(originalScale.x, originalScale.y / verticalScale, originalScale.z);
+
+        Vector3 scale = characterModelTransform.localScale;
+        scale.y /= verticalScale;
+        characterModelTransform.localScale = scale;
+    }
+
+
+    //Método para ver que el collider se encoje correctamente
+    private void OnDrawGizmosSelected()
+    {
+        if (characterCollider != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(characterCollider.bounds.center, characterCollider.radius * verticalScale);
+        }
+    }
+}
