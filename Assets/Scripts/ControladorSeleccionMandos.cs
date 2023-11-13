d967257a26558473eb4c64eaca27b17e7417e712
ANAVGAR <anavgar@etsinf.upv.es>
2023-11-13 16:54:25 +0100

    CarpetasOrdenadas

---
+++ Assets/Scripts/ControladorSeleccionMandos.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class ControladorSeleccionMandos : MonoBehaviour
+{
+    public InputDevice deviceP1;
+    public InputDevice deviceP2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
