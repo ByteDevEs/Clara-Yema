a9a83d0e5ed69c7d92f38678a60518fc00bcfacf
VonP2 <adrian11soriano@gmail.com>
2023-11-15 13:00:05 +0100

    Ahora pausan los dos +-
    Solo funciona al poner en uno de los pj el método de pausar asociado a si mismo y en un objeto vacío poner el método de pausar pero asociado al otro jugador

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -5,31 +5,76 @@ using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenuCanvas;
     private bool isPaused = false;
+    private bool isMenuOpen = false;
+
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        // Asegúrate de desactivar el menú de pausa al inicio
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
 
     public void OnStart(InputAction.CallbackContext context)
     {
-        if (isPaused)
+        if (context.started)
         {
-            ResumeGame();
-        }
-        else
-        {
-            PauseGame();
+            if (isPaused)
+            {
+                // Si ya está en pausa, reanudar el juego
+                ResumeGame();
+            }
+            else
+            {
+                // Si no está en pausa, pausar el juego y abrir el menú
+                PauseGame();
+            }
         }
     }
+    void Update()
+    {
+        
+
+        // Controlar el menú si está abierto
+        if (isMenuOpen)
+        {
+            // Aquí puedes agregar lógica para controlar el menú con el jugador que pulsó "Start"
+            // Puedes usar Input.GetAxis, Input.GetButton, etc., según tus necesidades
+            // Por ejemplo:
+            // float menuInput = Input.GetAxis("Vertical");
+            // Mover el cursor del menú en consecuencia
+        }
+    }
+
     void PauseGame()
     {
         isPaused = true;
-        Time.timeScale = 0f; // Pausa el tiempo del juego
-        pauseMenuCanvas.SetActive(true);
+
+        // Pausar el tiempo y mostrar el menú
+        Time.timeScale = 0f;
+
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(true);
+            isMenuOpen = true;
+        }
     }
 
-    public void ResumeGame()
+    void ResumeGame()
     {
         isPaused = false;
-        Time.timeScale = 1f; // Reanuda el tiempo del juego
-        pauseMenuCanvas.SetActive(false);
+
+        // Reanudar el tiempo y cerrar el menú
+        Time.timeScale = 1f;
+
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+            isMenuOpen = false;
+        }
     }
 }
