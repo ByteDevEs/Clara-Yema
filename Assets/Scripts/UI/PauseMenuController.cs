057988876987ba926b5796011cee24218298eb4c
VonP2 <adrian11soriano@gmail.com>
2023-11-19 20:22:41 +0100

    Menú de pausa +-
    Funciona, pero no salen los botones como seleccionados, ni en el menú de pausa ni en el menú principal cuando se pasa de uno a otro

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PauseMenuController : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
