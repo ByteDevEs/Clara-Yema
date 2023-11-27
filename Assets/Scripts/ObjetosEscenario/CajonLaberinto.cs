590fa757829865960c2cbb1bbb933cae9f59e194
VonP2 <adrian11soriano@gmail.com>
2023-11-27 13:23:25 +0100

    Tp laberinto ya funciona

--- Assets/Scripts/ObjetosEscenario/CajonLaberinto.cs
+++ Assets/Scripts/ObjetosEscenario/CajonLaberinto.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class CajonLaberinto : MonoBehaviour
 {
-    //Cambia la escena al laberinto
+    private int playersInside = 0; // Variable para contar los jugadores dentro del collider
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) { SceneManager.LoadScene(3); }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playersInside++; // Incrementa el contador cuando un jugador entra
+            Debug.Log("Player entered. Total players inside: " + playersInside);
+
+            if (playersInside >= 2) // Verifica si ambos jugadores están dentro
+            {
+                SceneManager.LoadScene(4); // Carga la escena
+            }
+        }
     }
+
+   
 }
