54759db29af76f0729e511cd622a566ad3b8895a
ANAVGAR <anavgar@etsinf.upv.es>
2023-11-30 12:26:03 +0100

    Update ActivarCercania.cs

--- Assets/Scripts/Dialogos/ActivarCercania.cs
+++ Assets/Scripts/Dialogos/ActivarCercania.cs
@@ -13,17 +13,12 @@ public class ActivarCercania : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        PlayerController[] playerControllers = new PlayerController[players.Length];
-        for (int i = 0; i < players.Length; i++)
-        {
-            playerControllers[i] = players[i].GetComponent<PlayerController>();
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         foreach (var player in players)
         {
             if(Vector3.Distance(player.transform.position, transform.position) < cercania)
@@ -33,6 +28,11 @@ public class ActivarCercania : MonoBehaviour
                     {
                         activated = true;
                         toActivate.SetActive(true);
+                        PlayerController[] playerControllers = new PlayerController[players.Length];
+                        for (int i = 0; i < players.Length; i++)
+                        {
+                            playerControllers[i] = players[i].GetComponent<PlayerController>();
+                        }
                         foreach (var p in playerControllers)
                         {
                             p.enabled = false;
