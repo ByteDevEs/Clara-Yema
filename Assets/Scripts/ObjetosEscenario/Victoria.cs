7eb1b3d271367176ce2700d3d4abff75679124c2
VonP2 <adrian11soriano@gmail.com>
2024-01-19 12:57:31 +0100

    Victoria

--- Assets/Scripts/ObjetosEscenario/Victoria.cs
+++ Assets/Scripts/ObjetosEscenario/Victoria.cs
@@ -6,26 +6,54 @@ public class Victoria : MonoBehaviour
 {
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    public GameObject ClaraWin;
+    public GameObject YemaWin;
 
     private void OnTriggerEnter(Collider other)
     {
         // Verifica si el objeto que entra en el collider es uno de los personajes
         if (other.CompareTag("Player"))
         {
-            // Teletransporta a ambos personajes a sus respectivos spawnPoints
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-            foreach (GameObject player in players)
+            if (other.name == "Clara")
             {
-                if (player.name == "Clara")
-                {
-                    player.transform.position = spawnPoint1.position;
-                }
-                else if (player.name == "Yema")
-                {
-                    player.transform.position = spawnPoint2.position;
-                }
+                ClaraWin.SetActive(true);
+            }
+            else if (other.name == "Yema")
+            {
+                YemaWin.SetActive(true);
+            }
+
+        }
+        StartCoroutine(TeletransportarYDesactivarDespuesDeEspera());
+    }
+
+    IEnumerator TeletransportarYDesactivarDespuesDeEspera()
+    {
+        yield return new WaitForSeconds(5f); // Espera 5 segundos
+
+        // Desactiva los objetos ganadores
+        ClaraWin.SetActive(false);
+        YemaWin.SetActive(false);
+
+        // Teletransporta a ambos personajes a sus respectivos spawnPoints
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject player in players)
+        {
+            if (player.name == "Clara")
+            {
+                player.transform.position = spawnPoint1.position;
+
+            }
+            else if (player.name == "Yema")
+            {
+                player.transform.position = spawnPoint2.position;
+
             }
         }
+
+
     }
 }
+
